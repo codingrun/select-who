12ac97f44bd0779c69de4001b56de1fcb90e09d9
Tuyoni <wangcodingrun@gmail.com>
2022-01-22 18:37:36 +0900

    tailwindcss추가

--- components/memberList/index.tsx
+++ components/memberList/index.tsx
@@ -5,7 +5,39 @@ const MemberList = ({ data }: { data: memberType[] }) => {
     <>
       <div>
         {data.map((item: memberType, index) => {
-          return <div key={index}>{item.option}</div>;
+          return (
+            <div key={index}>
+              <div>{item.option}</div>
+              <input
+                type="checkbox"
+                className="
+                          rounded
+                          border-gray-300
+                          text-indigo-600
+                          shadow-sm
+                          focus:border-indigo-300
+                          focus:ring
+                          focus:ring-offset-0
+                          focus:ring-indigo-200
+                          focus:ring-opacity-50
+                        "
+              />
+              <input
+                type="checkbox"
+                className="
+                          rounded
+                          border-gray-300
+                          text-indigo-600
+                          shadow-sm
+                          focus:border-indigo-300
+                          focus:ring
+                          focus:ring-offset-0
+                          focus:ring-indigo-200
+                          focus:ring-opacity-50
+                        "
+              />
+            </div>
+          );
         })}
       </div>
     </>
