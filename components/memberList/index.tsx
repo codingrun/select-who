7dfb1337bbc824371dfc11b70eebb98c6590e2dd
Tuyoni <wangcodingrun@gmail.com>
2022-01-22 22:59:08 +0900

    로직 변경

--- components/memberList/index.tsx
+++ components/memberList/index.tsx
@@ -21,7 +21,7 @@ const MemberList = ({
               선물여부
             </th>
             <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-center">
-              나?
+              룰렛돌리는자
             </th>
           </tr>
         </thead>
@@ -33,30 +33,12 @@ const MemberList = ({
                   {item.option}
                 </td>
                 <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
-                  {item.selected ? (
-                    <div>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-6 w-6"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                        />
-                      </svg>
-                    </div>
-                  ) : (
-                    <input
-                      type="checkbox"
-                      checked={item.isGift}
-                      onClick={() => checkIsGift(item)}
-                      disabled={item.selected}
-                      className="
+                  <input
+                    type="checkbox"
+                    checked={item.isGift}
+                    onClick={() => checkIsGift(item)}
+                    disabled={item.selected}
+                    className="
                           rounded
                           border-gray-300
                           text-indigo-600
@@ -67,8 +49,7 @@ const MemberList = ({
                           focus:ring-indigo-200
                           focus:ring-opacity-50
                         "
-                    />
-                  )}
+                  />
                 </td>
                 <td className="border-b border-slate-100 dark:border-slate-700 p-4 pr-8 text-slate-500 dark:text-slate-400 text-center">
                   <input
